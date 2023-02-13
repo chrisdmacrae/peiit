9b7acb3b8050684c8050eb2b69cf20ec898c998b
Christopher Macrae <chrisdmacrae@Christophers-MacBook-Pro-2.local>
2023-02-12 23:16:37 -0400

    Fix typography

--- src/content/typography.tsx
+++ src/content/typography.tsx
@@ -11,7 +11,7 @@ export const typography = {
   a: (props: any) => <a className="text-blue-500" {...props} />,
   ul: (props: any) => <ul className="list-disc list-outside marker:text-slate-500 dark:marker:text-slate-600 text-slate-900 dark:text-slate-50 pl-6 pt-2 mb-4" {...props} />,
   ol: (props: any) => <ol className="list-decimal list-outside text-slate-900 dark:text-slate-50 pl-6 pt-2 mb-4" {...props} />,
-  li: ({children, ...props}: any) => <li {...props}><Text>{children}</Text></li>,
+  li: ({children, ...props}: any) => <li {...props}><p>{children}</p></li>,
   blockquote: ({children, ...props}: any) => (
     <div className="border-l-4 pl-4 py-6 border-slate-200 dark:border-slate-700">
       <p {...props}>
