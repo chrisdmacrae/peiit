b515e0647a9e617ebfd9400850b45be7593e6d98
Christopher Macrae <chrisdmacrae@Christophers-MacBook-Pro-2.local>
2023-02-17 10:09:05 -0400

    Fix site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,6 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "peiit.vercel.app",
+  site: "https://peiit.vercel.app",
   integrations: [tailwind(), mdx(), react(), image()]
 });
